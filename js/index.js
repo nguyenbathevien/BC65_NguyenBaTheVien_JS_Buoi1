264caa85f69018f3c550852e9f2e27737585328f
NguyenBaTheVien <thevien2k3@gmail.com>
2024-01-30 21:53:01 +0700

    nop bai tap Git buoi 1

---
+++ js/index.js
@@ -0,0 +1,49 @@
+// Bai 1
+
+    var luong=2000
+    var ngay=2;
+    var tinhluong;
+    tinhluong=luong*ngay;
+    console.log('tinhluong: ', tinhluong);
+    
+
+// Bai 2
+
+    var number1=-1;
+    var number2=2;
+    var number3=3;
+    var number4=-5;
+    var number5=4;
+    var tinhTB;
+    TB = (number1+number2+number3+number4+number5)/5;
+    console.log('TrungBinh: ', TB);
+    
+
+// Bai 3
+
+    var usd = 2;
+    var tien;
+    tien = usd*23500;
+    console.log('tien: ', tien);
+  
+
+// Bai 4
+
+    var chieudai=4;
+    var chieurong=2;
+    var dientich,chuvi;
+    dientich=chieudai*chieurong;
+    console.log('dientich: ', dientich);
+    chuvi=(chieudai+chieurong)/2;
+    console.log('chuvi: ', chuvi);
+    
+
+
+//Bai 5
+
+    var kyso = -24;
+    var donvi= Math.abs(kyso % 10); 
+    var chuc = Math.trunc(kyso / 10); 
+    var sum = donvi + chuc;
+    console.log('sum : ', sum );
+    
